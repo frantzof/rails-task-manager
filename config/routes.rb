bb6e4a0b7e1b49f21c2c3a8667be50568e6d714e
Frantz Fortunat <frantzfortunat@gmail.com>
2020-08-11 15:08:43 +0200

    task manager with no css

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "tasks#index"
   get "tasks", to: "tasks#index"
+  get "tasks/new", to: "tasks#new"
+  get "tasks/:id", to: "tasks#show", as: "task"
+  post "tasks", to: "tasks#create"
+  get "tasks/:id/edit", to: "tasks#edit", as: "edit_task"
+  patch "tasks/:id", to: "tasks#update"
+  delete "tasks/:id", to: "tasks#destroy"
+  
+  # resources :tasks
 end
